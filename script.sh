11a9e4262e73fe6c49425404e74710fdb60da380
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-06-02 15:04:57 +0100

    updated bash file to remove folders at start

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+rm fol_1
+rm fol_2
 mkdir fol_1
 mkdir fol_2
 touch fol_1/1_1.txt
 touch fol_1/1_2.txt
 touch fol_1/1_3.txt
+touch fol_2/1_1.txt
+touch fol_2/1_2.txt
+touch fol_2/1_3.txt
