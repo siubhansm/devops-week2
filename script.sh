5428b247abf973ca23c528e10341960f077bfff4
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-06-02 16:04:32 +0100

    job completed text added

--- script.sh
+++ script.sh
@@ -10,3 +10,13 @@ touch fol_1/1_3.txt
 touch fol_2/1_1.txt
 touch fol_2/1_2.txt
 touch fol_2/1_3.txt
+cd fol_1
+chmod 711 1_1.txt
+chmod 711 1_3.txt
+chmod 777 1_2.txt
+cd ../
+cd fol_2
+chmod 711 1_1.txt
+chmod 711 1_3.txt
+chmod 777 1_2.txt
+echo "job completed"
